901a84bf9865c81271ca1842324d6fa0023ce3a7
dev-vin-dev <vincentmusyimi40@gmail.com>
2025-05-22 20:23:52 +0300

    Script that prints 3 lines

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,19 @@
+// Printing 3 lines
+/*const messages = [
+    "C is fun", 
+    "Python is cool",
+    "Javascript is amazing"];
+
+for (const message of messages){
+    console.log(message);
+}*/
+
+const messages = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (const message of messages) {
+  console.log(message);
+}
