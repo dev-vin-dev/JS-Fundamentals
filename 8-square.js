7a87048dc56d1321b1e1f8ca033b5ace4ec0887b
dev-vin-dev <vincentmusyimi40@gmail.com>
2025-05-22 20:53:22 +0300

    Prints a Square

---
+++ 8-square.js
@@ -0,0 +1,12 @@
+//Printing a Square
+
+const square = Number(process.argv[2]);
+if (Number.isInteger(square)){
+    let i=0;
+    while (i < square){
+        console.log("X".repeat(square))
+        i++
+    }
+}else {
+    console.log("Missing Size")
+};
